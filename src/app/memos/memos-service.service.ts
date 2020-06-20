054b408dc98297d1c078596c812f22a7931a7de8
husam287 <husam287@users.noreply.github.com>
2020-06-20 04:16:22 +0200

    edited the memos' leader message
    -make the container in the component itself

--- src/app/memos/memos-service.service.ts
+++ src/app/memos/memos-service.service.ts
@@ -4,8 +4,22 @@ export class MemosServiceService {
 
   
   //array of memories
-  memos=[
-    new memoModel('test',"it's a test memory hi from test",'https://bit.ly/31bypTZ',new Date(2018,3,27))
+  private memos=[
+    new memoModel('test',"it's a test memory hi from test",'https://bit.ly/31bypTZ',new Date(2018,3,27)),
+    new memoModel('Hossam',"hiii this is me hhhh hhhhhhh hhhhhhhhhh",'https://bit.ly/2NcIh7z',new Date(2019,4,12))
   ];
   constructor() { }
+
+  addMemo(memo:memoModel){
+    this.memos.push(memo);
+  }
+  getMemos(){
+    return this.memos.slice();
+  }
+  updateMemo(newMemo:memoModel,id:number){
+    this.memos[id]=newMemo;
+  }
+  removeMemo(id:number){
+    this.memos.splice(id);
+  }
 }
--- src/app/memos/my-memos/my-memos.component.ts
+++ src/app/memos/my-memos/my-memos.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { memoModel } from '../memo.model';
+import { MemosServiceService } from '../memos-service.service';
 
 @Component({
   selector: 'app-my-memos',
   templateUrl: './my-memos.component.html',
-  styleUrls: ['./my-memos.component.css']
+  styleUrls: ['./my-memos.component.css'],
+  providers:[MemosServiceService]
 })
 export class MyMemosComponent implements OnInit {
   memos=[];
-  constructor() { }
+  constructor(private memosService:MemosServiceService) { }
 
   ngOnInit(): void {
+    this.memos=this.memosService.getMemos();
   }
 
 }
