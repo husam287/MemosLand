07c5453f4103c33225bb6a44c7ff0148d5bf067c
husam287 <husam287@users.noreply.github.com>
2020-06-22 20:35:57 +0200

    adjust envirom. and angularfire2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC8A_P2j6nioUsvjQHfk1lPyq-sJ1086zU",
+    authDomain: "recipebook-fe6b3.firebaseapp.com",
+    databaseURL: "https://recipebook-fe6b3.firebaseio.com",
+    projectId: "recipebook-fe6b3",
+    storageBucket: "recipebook-fe6b3.appspot.com",
+    messagingSenderId: "406859359773",
+    appId: "1:406859359773:web:36317b2d35ee3ff58748f4",
+    measurementId: "G-02S8WZ8G72"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyC8A_P2j6nioUsvjQHfk1lPyq-sJ1086zU",
+    authDomain: "recipebook-fe6b3.firebaseapp.com",
+    databaseURL: "https://recipebook-fe6b3.firebaseio.com",
+    projectId: "recipebook-fe6b3",
+    storageBucket: "recipebook-fe6b3.appspot.com",
+    messagingSenderId: "406859359773",
+    appId: "1:406859359773:web:36317b2d35ee3ff58748f4",
+    measurementId: "G-02S8WZ8G72"
+  }
 };
 
 /*
